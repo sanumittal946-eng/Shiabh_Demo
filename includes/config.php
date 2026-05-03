6aabddbe10cd5805e47784965f9ec5fd715cc446
Somil <somilmittal946@gmail.com>
2026-05-03 23:31:15 +0530

    Enable error reporting for live debugging

--- includes/config.php
+++ includes/config.php
@@ -23,8 +23,8 @@ if ($is_local) {
     define('DB_USER', 'if0_41820914'); 
     define('DB_PASS', 'YqQOYtWuz3e'); 
     
-    error_reporting(0);
-    ini_set('display_errors', 0);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 }
 
 // Application Constraints
